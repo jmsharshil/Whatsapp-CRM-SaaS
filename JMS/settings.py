7b613b6cd14830a0dd35ea7827446b720682567b
Hemish123 <consultant@jmsadvisory.in>
2026-08-25 14:22:23 +0530

    logs added

--- JMS/settings.py
+++ JMS/settings.py
@@ -300,3 +300,30 @@ META_PERMANENT_TOKEN = "EAA3kk7FBaZCIBRejscriGIa8PZCsqMZAVPYdLsZC9ZAep1NuDmZBZAK
 VERIFY_TOKEN = os.environ.get("VERIFY_TOKEN", "123")
 
 SHOPIFY_ACCESS_TOKEN = os.getenv("SHOPIFY_ACCESS_TOKEN", "")
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(asctime)s %(levelname)s %(name)s %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
